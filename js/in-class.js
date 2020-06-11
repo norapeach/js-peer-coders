49141eb44133164d570019d7ee59653a4de26b00
Kate <kbaldwin702@gmail.com>
2020-06-11 15:24:49 -0700

    added fade in to inputs

--- js/in-class.js
+++ js/in-class.js
@@ -1,7 +1,18 @@
 
-(function () {
-  $('.service').hide();
-  $('.people').hide();
+$(function () {
+    $('.service').hide();
+    $('.people').hide();
 
- 
-})();
+    $('#currency').on('input', function(){
+      $('.service').fadeIn(800);
+    });
+
+    $('.service').on('input', function(){
+      $('.people').fadeIn(800);
+    });
+
+    $('.people').on('input', function(){
+      $('input:submit').fadeIn(800);
+    });
+});
+
