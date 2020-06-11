2779e0112b593106cf9db6ae48f10631ecbe110a
Nora P <veganmuffcakes@gmail.com>
2020-06-11 13:25:33 -0700

    Initial commit with existingproject structure

---
+++ js/in-class.js
@@ -0,0 +1,35 @@
+// Accordian script
+$( function() {
+    $( "#accordion" ).accordion();
+  } );
+
+// Datepicker script
+$( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+
+// Toggle Effect script
+$( function() {
+    // run the currently selected effect
+    function runEffect() {
+      // get effect type from
+      var selectedEffect = $( "#effectTypes" ).val();
+ 
+      // Most effect types need no options passed by default
+      var options = {};
+      // some effects have required parameters
+      if ( selectedEffect === "scale" ) {
+        options = { percent: 50 };
+      } else if ( selectedEffect === "size" ) {
+        options = { to: { width: 200, height: 60 } };
+      }
+ 
+      // Run the effect
+      $( "#effect" ).toggle( selectedEffect, options, 500 );
+    };
+ 
+    // Set effect from select menu value
+    $( "#button" ).on( "click", function() {
+      runEffect();
+    });
+  } );
