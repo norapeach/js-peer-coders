1cf3db0677a72ee3f72193a7ddaaf69d145ba4b1
Nora P <veganmuffcakes@gmail.com>
2020-06-19 02:50:33 -0700

    issue-incomplete: add start to calcTip method with pseudocode comments

--- js/tip-calculator.js
+++ js/tip-calculator.js
@@ -7,6 +7,7 @@
 //// GLOBAL VARIABLES ////
 var tipForm = document.querySelector('form'); // the containing element, the submit input will act as the triggering event, making the form the event object that can be traversed
 
+//// FUNCTIONS ////
 $(function () { // IFFE 
 
     $('.service').hide();
@@ -27,8 +28,30 @@ $(function () { // IFFE
 
 });
 
-//// FUNCTIONS ////
 /* EVENT FUNCTION: logic for tip calculation to be displayed on the same page */
 function calcTip(event) {
   event.preventDefault();     // prevent default behavior of form 'submit'; keeps browser from leaving current page
+
+  var form = event.target; // local reference to form Node
+
+  var billInput = parseInt(form.querySelector('#currency').value); // get the value and parse to integer (or NaN) 
+  // TODO: the #currency input is type=number - issue for currency?s
+  var serviceInput = form.querySelector('#service').value; // references option selected by user
+
+  var peopleInput = parseInt(form.querySelector('#people').value); // TODO: change input to type="number" ?
+
+
+
+  //TODO: evaluate value and set tip percentage based on service
+    // excellent 20%
+    // good: 15 %
+    // fair: 15 %; leave a note
+    // needs improvement: 15% open yelp to restaurant page to leave a review?
+
+  // TODO: validate form prior to calculation
+    // bill can't be negative
+    // select has to be chosen before calculation 
+    // party of people has to be between 1-10 
+        //  10 require 20% tip per person.
+
 }
