201c5e40f599bb37b5147a00090e57bd75ca9217
Nora P <veganmuffcakes@gmail.com>
2020-06-19 23:44:27 -0700

    add version of calcTip function - method is untested - no display in current version

--- js/tip-calculator.js
+++ js/tip-calculator.js
@@ -1,17 +1,75 @@
 /*******************************************************
 @author Nora P.
 @version 6/18/20
- * ASSN8 REQUIREMENTS:
- * SCC WEB150 SpQ2020
+ * SCC WEB150 SpQ2020 ASSN8 REQUIREMENTS:
+ * Code a web page that demonstrates a minimum of 4 JQuery * interactions, widgets, or effects 
+ * 
+ * Incomplete development version of a unique tip
+ * calculator utilizing
+ * jQUery UI effects, themes and widgits
+ * 
+ *  TODO: complete commented pseudo code
  ***********************************************/
 //// GLOBAL VARIABLES ////
 var tipForm = document.querySelector('form'); // the containing element, the submit input will act as the triggering event, making the form the event object that can be traversed
 
 //// FUNCTIONS ////
-$(function () { // IFFE 
+function getTipPercent(value, tipPercent, message) {
+  /***
+   * Parameters: 
+   * value =the option value selected by the user
+   * tipPercent = existing variable to be modified and passed back to calcTip function
+   * message = variable to store content about the importance of tipping to pass back to page 
+   ***/
 
+  // switch: evaluate user selection and modify the tipPercent and message assignments accordingly - then return to tipCalc()
+  switch (value) {
+    case "ex":
+      tipPercent = 0.25;
+      message = "Something positive about appreciating good service."
+      break;
+    case "good":
+      tipPercent = 0.20;
+      message = "Info about origin of 15% as a tip and why more is encouraged"
+      break;
+    case "fair":
+      // default tipPercent unchanged
+      message = "Even fair service deserves a living wage";
+      break;
+    case "bad":
+      // default tipPercent unchanged
+      message = "Prompt that service should still be acknowledged. Something about leaving a yelp review - delay, page will be embedded?";
+      break;
+    // no default necessary  
+  } // end switch 
+} // end function getTipPercent
+
+function isLargeParty(value, tipPercent, message) {
+   /***
+   * Parameters: 
+   * value = the number of people entered by the user
+   * tipPercent = existing variable to be modified and passed back to calcTip function
+   * message = variable to add content about larger group size dynamically before calculation (TODO)
+   * Return: a boolean if a larger party
+   ***/
+  var isLarge = true; 
+  if (value > 10 && value < 20) {
+    tipPercent = 0.25;
+    message += "append content about importance of tipping with larger groups";
+    return isLarge;
+  } else if (value >= 20) {
+    tipPercent = 0.30;
+    message += "append content - larger groups, event planning. Resources required deserve additional compensation.";
+    return isLarge
+  }
+  return isLarge = false;  // no default or else - allows for values less than 10 to be determined by getTipPercent
+ 
+} // end function
+
+$(function () { // IFFE 
     $('.service').hide();
     $('.people').hide();
+    // TODO: add query for asside to hide then show once calculation is complete? or build dynamically?
 
     $('#currency').on('input', function(){
       $('.service').fadeIn(800);
@@ -25,33 +83,38 @@ $(function () { // IFFE
       $('input:submit').fadeIn(800);
     });
 
-
-});
+    tipForm.addEventListener("submit", calcTip, false); /* Attaches Event listener to the Form  for the click on the submit button
+    boolean relates to event propagation: false --> bubbling: inner most element's event is handled first
+    */
+}); // END IFFE ? Q: do the functions go inside this?
 
 /* EVENT FUNCTION: logic for tip calculation to be displayed on the same page */
 function calcTip(event) {
   event.preventDefault();     // prevent default behavior of form 'submit'; keeps browser from leaving current page
 
+  var tipPercent = 0.15 
+  /* Default 15% tip is passed regardless of user input --> this is the point. Depending on <option value='x'> return default may change */
+
+  var message = ""; 
+  /* default empty string; will store info about tipping to dispay */
+
+  // ACCESS DOM TREE via EVENT OBJECT // 
   var form = event.target; // local reference to form Node
 
-  var billInput = parseInt(form.querySelector('#currency').value); // get the value and parse to integer (or NaN) 
-  // TODO: the #currency input is type=number - issue for currency?s
+  var billInput = form.querySelector('#currency').value; // removed parseInt - HTML has input validation constraint for type="number"
+
   var serviceInput = form.querySelector('#service').value; // references option selected by user
 
-  var peopleInput = parseInt(form.querySelector('#people').value); // TODO: change input to type="number" ?
+  getTipPercent(serviceInput, tipPercent, message); // function below - validates selection options and modifies tipPercent / message assignments
 
+  var peopleInput = form.querySelector('#people').value; // Q: change input to type="number" don't have to parseInt since input is now number? 
 
+  var isLarge = isLargeParty(peopleInput, tipPercent, message); // modifies parameter values before returning boolean TODO: use in display <aside>
 
-  //TODO: evaluate value and set tip percentage based on service
-    // excellent 20%
-    // good: 15 %
-    // fair: 15 %; leave a note
-    // needs improvement: 15% open yelp to restaurant page to leave a review?
+  var totalTip = billInput * tipPercent; // calculate total tip (cached to append to DOM Tree)
 
-  // TODO: validate form prior to calculation
-    // bill can't be negative
-    // select has to be chosen before calculation 
-    // party of people has to be between 1-10 
-        //  10 require 20% tip per person.
+  var ppTipAmount = totalTip / peopleInput; // calc per person Tip amount - total tip (cached to append to DOM Tree)
 
-}
+  // display total with effect
+} // end function calcTip
+
